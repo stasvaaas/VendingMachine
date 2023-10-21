f14a68e37d1f7522d3e256c73ee95bce86c8411a
stasvaaas <105922844+stasvaaas@users.noreply.github.com>
2023-10-21 11:30:43 +0300

    Add project files.

---
+++ VendingMachine/Entities/Cup.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VendingMachine.Contracts;
+
+namespace VendingMachine.Entities
+{
+    internal class Cup : CupBase, ICoffee, IMilk, ISugar, IWater
+    {
+        public override void Stir()
+        {
+            Console.WriteLine("Stirring the coffee.");
+        }
+        public void AddSugar()
+        {
+            Console.WriteLine("Adding sugar");
+        }
+
+        public void AddMilk()
+        {
+            Console.WriteLine("Adding milk");
+        }
+
+        public void AddWater()
+        {
+            Console.WriteLine("Adding water");
+        }
+
+        public void AddCoffee()
+        {
+            Console.WriteLine("Adding coffee");
+        }
+    }
+}
---
+++ VendingMachine/Program.cs
@@ -0,0 +1,44 @@
+﻿using VendingMachine.Contracts;
+using VendingMachine.Entities;
+
+namespace VendingMachine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cup coffeeCup = new Cup();
+
+            AddCoffee(coffeeCup);
+            coffeeCup.Stir();
+            AddWater(coffeeCup);
+            coffeeCup.Stir();
+            AddMilk(coffeeCup);
+            coffeeCup.Stir();
+            AddSugar(coffeeCup);
+            coffeeCup.Stir();
+
+            Console.WriteLine("Your favorite coffee is ready!");
+        }
+
+        static void AddCoffee(ICoffee container)
+        {
+            container.AddCoffee();
+        }
+
+        static void AddWater(IWater container)
+        {
+            container.AddWater();
+        }
+
+        static void AddMilk(IMilk container)
+        {
+            container.AddMilk();
+        }
+
+        static void AddSugar(ISugar container)
+        {
+            container.AddSugar();
+        }
+    }
+}
